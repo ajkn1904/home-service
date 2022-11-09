0cb8a65a9e0aeed3eb461867e166e99ebce07f38
ajkn1904 <ajknishat@gmail.com>
2022-11-10 04:55:38 +0600

    my review page added

---
+++ src/components/MyReviewCard/MyReviewCard,/MyReviewCard.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+
+const MyReviewCard = ({review}) => {
+    const [serviceData, setServiceData] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${review.serviceInfo}`)
+        .then(res => res.json())
+        .then(data => setServiceData(data))
+    }, [])
+    const {name, img} = serviceData
+
+    return (
+        <div className="card w-96 bg-red-100 shadow-xl mx-auto">
+                    <div className="card-body">
+                        
+                        <div className="flex justify-between mb-4">
+                            
+                            <div className='flex justify-center items-center'>
+                                <div className="avatar mr-3">
+                                    <div className="w-10 h-10 rounded-full ring ring-offset-base-100 ring-offset-0">
+                                        <img src={review.usersImg} alt=''/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <small>{review.userName}</small>
+                                    <br/>
+                                    <strong>{review.email}</strong>
+
+                                </div>
+                            </div>
+                            <div>
+                                <button className="btn btn-circle btn-error">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className='bg-white p-3 rounded-md my-2'>
+                            <p>Service Name: <span className='font-semibold'>{name}</span></p>
+                            <hr className='my-4'/>
+                            <p>{review.text}</p>
+                            <div className='ml-64'>
+                                <button className='btn btn-ghost btn-circle'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                        </div>
+
+                        <div className="card-actions justify-end">
+                        <span className="badge badge-warning mx-2">{review.ratings}</span>
+                        <div className="badge badge-outline">Good</div> 
+                        </div>
+                    </div>
+        </div>
+    );
+};
+
+export default MyReviewCard;
--- src/components/MyReviews/MyReviews.js
+++ src/components/MyReviews/MyReviews.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Context/AuthProvider/AuthProvider';
+import MyReviewCard from '../MyReviewCard/MyReviewCard,/MyReviewCard';
 
 const MyReviews = () => {
+    const {user} = useContext(AuthContext);
+    const [myreview, setMyreview] = useState([])
+
+    const url = `http://localhost:5000/myreviews?email=${user.email}`
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setMyreview(data))
+    }, [user?.email])
+
+
     return (
         <div>
-            <h1>Here are my review</h1>
+            <h1 className='text-center text-3xl font-bold my-20'>My All Reviewes ({myreview.length})</h1>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mx-12 sm:mx-12 md:mx-14 lg:mx-20 my-20'>
+                {
+                    myreview.map(review => <MyReviewCard review={review} key={review._id}></MyReviewCard>)
+                }
+            </div>
+
         </div>
     );
 };
